b54ac89b8d08df1aa5a04928eae6b51399c17a28
Demix88 <skrasnauskis@gmail.com>
2022-03-22 13:06:07 +0000

    quick edits

--- ClassesAndMethodsAssignment2/Program.cs
+++ ClassesAndMethodsAssignment2/Program.cs
@@ -8,8 +8,20 @@ namespace ClassesAndMethodsAssignment2
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+            int x = Math.MySumInt(399);
+            Console.WriteLine("399 +optional value = " + x);
+            Console.WriteLine("Please type in a first number ");
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You don\'t need to enter 2nd value");
+            //Console.WriteLine("Please type in second Number (optional)");
+            // Im not sure if need this code or not
+            // int n = Convert.ToInt32(Console.ReadLine());
+
+            int y = Math.MyTimesInt(m);
+            Console.WriteLine( "result: "+y);
+            Console.ReadLine();
         }
     }
 }
